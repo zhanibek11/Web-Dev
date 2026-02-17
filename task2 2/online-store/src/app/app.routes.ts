6fc2afc45378d202c5c71a5090d6fb20bbe14c9f
zhanibek11 <janibekk042@gmail.com>
2026-02-18 01:48:44 +0500

    Delete task2 2 directory

--- task2 2/online-store/src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { ProductListComponent } from './components/product-list/product-list.component';
-
-export const routes: Routes = [
-  { path: '', component: ProductListComponent },
-];
