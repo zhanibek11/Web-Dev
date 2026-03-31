1a06e711cf4bcf33204919ebced51c18d7ec5d39
zhanibek11 <janibekk042@gmail.com>
2026-03-31 22:36:39 +0500

    Add lab8

---
+++ shopback/api/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from api.models import productModel,categoryModel
+
+# Register your models here.
+admin.site.register(productModel)
+admin.site.register(categoryModel)
