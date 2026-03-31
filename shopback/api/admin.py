ad52244a7d8a0c1682909724bc177057f3104e8d
zhanibek11 <janibekk042@gmail.com>
2026-03-31 23:17:51 +0500

    Delete shopback directory

--- shopback/api/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from api.models import productModel,categoryModel
-
-# Register your models here.
-admin.site.register(productModel)
-admin.site.register(categoryModel)
